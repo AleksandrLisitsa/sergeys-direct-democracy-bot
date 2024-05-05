7e11aae0819b671a39a95daf45818df56408903e
Aleksandr <lelisitsaa@mail.ru>
2024-05-05 14:00:57 +0300

    fix

--- cogs/change_banner.py
+++ cogs/change_banner.py
@@ -12,7 +12,7 @@ class ChangeIcon(commands.Cog):
     async def change_banner(self,
                     ctx : discord.TextChannel | discord.VoiceChannel | discord.StageChannel):
         choice = await vote(self.bot, ctx=ctx, title=f"Поставить ли новую аву?", options=["Да", "Нет"],
-                        symbols='letters', importance=Importance.medium)
+                        symbols='thumbs', importance=Importance.medium)
         if choice.pop() == 1:
             await ctx.send("Голосование провалилось")
         else:
